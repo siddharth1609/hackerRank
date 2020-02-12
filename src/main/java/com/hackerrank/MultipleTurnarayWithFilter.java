f419ac5592767e1999f9e1349af4bedcf1cd23f4
siddharth1609 <siddharthsonwane18@gmail.com>
2020-02-12 23:27:06 +0530

    multile turnary operator in filter

---
+++ src/main/java/com/hackerrank/MultipleTurnarayWithFilter.java
@@ -0,0 +1,53 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+public class MultipleTurnarayWithFilter {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Enter Size of array:");
+		
+		int n = sc.nextInt();
+		
+		int arr[] = new  int[n];
+		
+		System.out.println("Enter array elelemt:");
+		
+		for(int i=0; i<arr.length;i++) {
+			
+			arr[i] = sc.nextInt();
+			
+			
+		}
+		
+	     for(int i =0 ;i<arr.length;i++) {
+		  
+	    	 if(arr[i] % 3==0 && arr[i] % 5==0) {
+				   
+				   System.out.println("FizzBUss");
+				   
+			   } 
+	    	 
+	    	 else if(arr[i] % 3==0) {
+			   System.out.println("Fizz");
+			   
+		   }else if(arr[i] % 5==0) {
+			   
+			   System.out.println("BIzz");
+		   }else {
+			   
+			   System.out.println(":"+arr[i]);
+		   }
+		   
+		  
+		  
+		  
+	  }
+		
+		
+	}
+
+}
